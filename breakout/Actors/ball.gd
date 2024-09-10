cb3010c50f2be7153314f8a07db2f54f8a11ded2
Jeb Davis <jebemery@gmail.com>
2024-09-09 22:43:01 -0600

    visuals added/particles modified

--- breakout/Actors/ball.gd
+++ breakout/Actors/ball.gd
@@ -13,6 +13,7 @@ const PADDLE_WIDTH: float = 100.0
 var current_score: int = 0
 var is_running = false
 var hit_sound = preload("res://Assets/cash-register-purchase-87313.mp3")
+var cash_part = load("res://Actors/collision_particles.tscn")
 
 
 func play_new_sound(sound: AudioStream):
@@ -21,15 +22,24 @@ func play_new_sound(sound: AudioStream):
 	new_audio_player.stream = sound
 	new_audio_player.play()
 	
+func collision_particle(col_position: Vector2):
+	var collision_cpu_particles = cash_part.instantiate()
+	get_tree().root.add_child(collision_cpu_particles)
+	collision_cpu_particles.position = col_position
+	collision_cpu_particles.emitting = true
+	
 
 func _ready() -> void:
 	#velocity = Vector2(1,1).normalized()
 	pass
 
 func _physics_process(delta: float) -> void:
+	rotation = forward.angle()
+	
 	#end_game: check if all bricks have been hit
 	if $"../Bricks".get_child_count() == 0:
 		is_running = false
+		get_child(0).visible = false
 		start_label.text = "[center]You Win!\nClick to Play Again.[/center]"
 		start_label.visible = true
 		game_ended.emit()
@@ -50,6 +60,7 @@ func _physics_process(delta: float) -> void:
 
 		if collision.get_collider().is_in_group("Bricks"):
 			collision.get_collider().queue_free()
+			collision_particle(collision.get_position())
 			current_score += 10
 			score_label.text = "Score: " + str(current_score)
 			collision_occurred.emit()
@@ -58,12 +69,14 @@ func _physics_process(delta: float) -> void:
 		if collision.get_collider().is_in_group("Paddle"):
 			var paddle_x = collision.get_collider().position.x -  PADDLE_WIDTH/2
 			var pos_on_paddle = (position.x - paddle_x) / PADDLE_WIDTH
-			print("Hit the paddle!" + str(pos_on_paddle))
+			#print("Hit the paddle!" + str(pos_on_paddle))
 			var bounce_angle = lerp(-PI * .8, -PI * .2, pos_on_paddle)
 			forward = Vector2.from_angle(bounce_angle)
 			
 		if collision.get_collider().is_in_group("game_over"):
 			is_running = false
+			get_child(0).visible = false
+			get_child(2).emitting = false
 			start_label.visible = true
 			start_label.text = "[center]GAME OVER[/center]"
 			await get_tree().create_timer(1.0).timeout
--- breakout/cpu_particles_2d.gd
+++ breakout/cpu_particles_2d.gd
@@ -3,7 +3,7 @@ extends CPUParticles2D
 
 func _on_ball_collision_occurred():
 	amount += 10
-	emission_sphere_radius += 3
+	emission_sphere_radius += .5
 
 
 func _on_ball_game_ended():
